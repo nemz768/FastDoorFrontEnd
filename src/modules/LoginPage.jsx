a6fde01e045d7977012dc65427d920dcf276b2aa
Pavel <pavel20042019@mail.ru>
2025-04-12 20:05:52 +0700

    loginCheck

--- src/modules/LoginPage.jsx
+++ src/modules/LoginPage.jsx
@@ -1,19 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles/login.css'
 
 
 const LoginPage = () => {
+
+    const [InputValue, setInputValue] = useState({
+        inputUser: '',
+        inputPass: '',
+    });
+
+    function sendToBack() {
+            fetch("http://localhost:8080/api/login", {
+                method: 'POST',
+            })
+                .then((res) => res.text())
+                .then((data) => setInputValue(data))
+                .catch((err) => console.error(err));
+    }
+
     return (
         <div className="login-page">
             <div className="login-section">
                 <h1 className="loginTitle">Вход</h1>
                 <form className="loginForm" method="POST" id="loginForm">
-                    <input className="shadows-input" placeholder="Логин" type="text" name="username" id="username"/>
-                    <input className="shadows-input" placeholder="Пароль" type="password" name="password"
-                           id="password"/>
+                    <input value={InputValue.inputUser} onChange={
+                        (e) => {
+                            setInputValue({
+                                ...InputValue,
+                                inputUser: e.target.value
+                            })
+                        }
+                    } className="shadows-input"  placeholder="Логин" type="text" name="username" id="username"/>
+                    <input  className="shadows-input" value={InputValue.inputPass} onChange={
+                        (e) => {
+                            setInputValue({
+                                ...InputValue,
+                                inputPass: e.target.value
+                            })
+                        }
+                    } placeholder="Пароль" type="password" name="password" id="password"/>
+
                     <p><a>Нажми</a> для регистрации</p>
+
                     <p><input className="checkbox" type="checkbox" name="rememberMe"/> Запомнить меня</p>
-                    <button className="button-login shadowsSection" type="submit">Войти</button>
+
+                    <button onClick={(e) => {
+                        e.preventDefault();
+                        sendToBack()
+                    }} className="button-login shadowsSection" >Войти</button>
+
                 </form>
             </div>
         </div>
--- src/modules/MainPage.jsx
+++ src/modules/MainPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Header} from "./Header.jsx";
 import {Footer} from "./Footer.jsx";
+import Main from "./Main.jsx";
 
 const MainPage = () => {
     return (
      <>
          <Header />
+         <Main/>
          <Footer />
      </>
     );
