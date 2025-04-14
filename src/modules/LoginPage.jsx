e2020618c6d6e71b09451d164ffa99cb369e1797
Pavel <pavel20042019@mail.ru>
2025-04-14 20:29:00 +0700

    login

--- src/modules/LoginPage.jsx
+++ src/modules/LoginPage.jsx
@@ -13,7 +13,7 @@ const LoginPage = () => {
     });
 
     function sendToBack() {
-            fetch("http://localhost:8080/api/login", {
+            fetch("http://127.0.0.1:8080/api/login", {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
