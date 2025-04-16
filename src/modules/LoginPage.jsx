4ee2ffd74f2e9218534f70125e15c891eb00ddc2
Pavel <pavel20042019@mail.ru>
2025-04-16 19:34:35 +0700

    test useRef login

--- src/modules/LoginPage.jsx
+++ src/modules/LoginPage.jsx
@@ -1,61 +1,49 @@
-import React, {useState} from 'react';
+import React, {useRef} from 'react';
 import '../styles/login.css'
 import {Link} from "react-router-dom";
 
 
 const LoginPage = () => {
-    const [InputValue, setInputValue] = useState({
-        inputUser: '',
-        inputPass: '',
-    });
+    const UsernameRef = useRef(null);
+    const PasswordRef = useRef(null);
 
-    function sendToBack()
-    {
-        console.log(InputValue)
-            fetch("/api/login", {
+    const sendToBack = async (e) => {
+        e.preventDefault();
+        console.log(UsernameRef.current.value)
+        console.log(PasswordRef.current.value)
+        const login = UsernameRef.current.value;
+        const password = PasswordRef.current.value;
+            await fetch("/api/login", {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    username: InputValue.inputUser,
-                    password: InputValue.inputPass
+                    username: login,
+                    password: password
                 })
             })
                 .then((res)=> res.json())
                 .then((data) => console.log('Server response: ', data.message))
                 .catch((err) => console.error(err));
+        UsernameRef.current.value = '';
+        PasswordRef.current.value = '';
+
     }
     return (
         <div className="login-page">
             <div className="login-section">
                 <h1 className="loginTitle">Вход</h1>
-                <form className="loginForm" method="POST" id="loginForm">
-                    <input value={InputValue.inputUser} onChange={
-                        (e) => {
-                            setInputValue({
-                                ...InputValue,
-                                inputUser: e.target.value
-                            })
-                        }
-                    } className="shadows-input"  placeholder="Логин" type="text" name="username" id="username"/>
-                    <input  className="shadows-input" value={InputValue.inputPass} onChange={
-                        (e) => {
-                            setInputValue({
-                                ...InputValue,
-                                inputPass: e.target.value
-                            })
-                        }
-                    } placeholder="Пароль" type="password" name="password" id="password"/>
+                <form onSubmit={sendToBack} className="loginForm" method="POST" id="loginForm">
+                    <input ref={UsernameRef} className="shadows-input"  placeholder="Логин" type="text" name="username" id="username"/>
+                    <input ref={PasswordRef} className="shadows-input" placeholder="Пароль" type="password" name="password" id="password"/>
 
                     <p><a>Нажми</a> для регистрации</p>
 
                     <p><input className="checkbox" type="checkbox" name="rememberMe"/> Запомнить меня</p>
 
-                 <Link to="/check"><button onClick={() => {
-                     sendToBack()
-                 }} className="button-login shadowsSection" type='submit' >Войти</button>
-                 </Link>
+                <button className="button-login shadowsSection" type='submit' >Войти</button>
+
                 </form>
             </div>
         </div>
