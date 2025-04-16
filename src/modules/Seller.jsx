14b3384a4e0f4ca2e0d382e5d17c172669de501c
Pavel <pavel20042019@mail.ru>
2025-04-16 20:43:08 +0700

    seller security

--- src/modules/Seller.jsx
+++ src/modules/Seller.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Header} from "./Header.jsx";
 import {Footer} from "./Footer.jsx";
 import '../styles/seller.css'
 
 export const Seller = () => {
+
+
+    useEffect(() => {
+        fetch("/api/orders/create", {
+            method: 'GET',
+            credentials: 'include',
+        })
+            .then((res) => res.text())
+            .then((data) => console.log(data))
+            .catch((err) => console.error(err));
+    }, [])
+
     return (
         <>
             <Header />
