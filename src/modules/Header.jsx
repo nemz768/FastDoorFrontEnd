f42b280839a991a3ec9dd870cebc1221374ea501
Pavel <pavel20042019@mail.ru>
2025-04-16 22:30:16 +0700

    createSellerPage -> pikaday

--- src/modules/Header.jsx
+++ src/modules/Header.jsx
@@ -18,7 +18,7 @@ export const Header = () => {
         <header className="header">
             <img src={logo} alt="dverka"/>
             <p>{message}</p>
-            zxcxzcxz
+            zxcxzcxzhgjyv
         </header>
     );
 };
