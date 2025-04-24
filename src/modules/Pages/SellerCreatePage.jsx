61a802962cef519498376279f00710ca292f52bc
Pavel <pavel20042019@mail.ru>
2025-04-24 23:03:05 +0700

    donePage try

--- src/modules/Pages/SellerCreatePage.jsx
+++ src/modules/Pages/SellerCreatePage.jsx
@@ -135,8 +135,8 @@ export const SellerCreatePage = () => {
 
     return (
         <div className="sellerCreatePage">
-            <form onSubmit={()=> {
-                sendResultsCreate();
+            <form onSubmit={(e)=> {
+                sendResultsCreate(e);
             }} method="POST" className="form-container">
                 <h1>Заполните данные о заказе</h1>
                 <h3 className='subtitleInput'>Укажите данные заказчика</h3>
