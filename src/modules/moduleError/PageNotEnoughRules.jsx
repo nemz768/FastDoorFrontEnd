8647f70105140df004d84c382a2a91b1040fb18a
Pavel <pavel20042019@mail.ru>
2025-05-01 21:20:06 +0700

    checking

--- src/modules/moduleError/PageNotEnoughRules.jsx
+++ src/modules/moduleError/PageNotEnoughRules.jsx
@@ -1,12 +1,15 @@
 import '../../styles/stylesError/pageNotEnoughRules.css';
+import {useNavigate} from "react-router-dom";
 
 export const PageNotEnoughRules = () => {
+    const navigate = useNavigate();
+
     return (
             <div className="page_403">
                 <div className="page_box">
                     <h1>403</h1>
                     <p>Извините, но Вы вышли за рамки дозволенного!</p>
-                    <p><a href="javascript:history.back()">Пожалуйста вернитесь назад и больше сюда не лезьте!</a></p>
+                    <p><a onClick={()=> navigate(-1)}>Пожалуйста вернитесь назад и больше сюда не лезьте!</a></p>
                 </div>
             </div>
     );
