25ed0607660e6a0913f4dc4a89a3887c9f92a5a1
Pavel <pavel20042019@mail.ru>
2025-05-01 21:53:06 +0700

    checking

--- src/modules/Pages/SellerCreatePage.jsx
+++ src/modules/Pages/SellerCreatePage.jsx
@@ -81,7 +81,6 @@ export const SellerCreatePage = () => {
             inInput.removeEventListener('input', () => handleInput(inInput));
         };
     }, []);
-
     useEffect(() => {
         if (refs.dateRef.current) {
                  new Pikaday({
