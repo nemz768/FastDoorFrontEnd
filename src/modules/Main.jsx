3fb394ee305794412b787df16fd254783299bfe3
Pavel <pavel20042019@mail.ru>
2025-05-03 13:43:06 +0700

    someFixes + overscrollbgc

--- src/modules/Main.jsx
+++ src/modules/Main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import "../App.css";
 import {Popup} from "./special/Popup.jsx";
+import '../styles/main.css'
 export const Main = () => {
     return (
-        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <div className="main">
         Main
             <Popup />
        </div>
