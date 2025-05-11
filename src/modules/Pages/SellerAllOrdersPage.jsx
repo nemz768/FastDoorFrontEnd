b9295e31d324839dbaf2194743b377f8d1ec2d91
Pavel <pavel20042019@mail.ru>
2025-05-11 16:11:12 +0700

    tableStyles + Modal

--- src/modules/Pages/SellerAllOrdersPage.jsx
+++ src/modules/Pages/SellerAllOrdersPage.jsx
@@ -18,7 +18,7 @@ export const SellerAllOrdersPage = () => {
             setIsLoading(true);
             setError(null);
             try {
-                const response = await fetch(`/api/list/sellerList?page=${currentPage}`, {
+                const response = await fetch(`/api/list/sellerList?id=${currentPage}`, {
                     method: 'GET',
                     headers: {
                         'Content-Type': 'application/json',
