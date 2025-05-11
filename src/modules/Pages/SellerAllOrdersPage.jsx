0583a586e80214d4401e3d47aba0a36d6c8a662c
Pavel <pavel20042019@mail.ru>
2025-05-11 14:02:01 +0700

    api orders test

--- src/modules/Pages/SellerAllOrdersPage.jsx
+++ src/modules/Pages/SellerAllOrdersPage.jsx
@@ -1,57 +1,24 @@
-import React, {useState, useEffect} from 'react';
-import {Header} from "../Header.jsx";
-import {Footer} from "../Footer.jsx";
-import "../../styles/stylePages/SellerAllOrdersPage.css"
-
+import React from 'react';
+import '../../styles/stylePages/SellerAllOrdersPage.css';
 
 export const SellerAllOrdersPage = () => {
-    const [orders, setOrders] = useState([]);
-    const [error, setError] = useState(null);
-    const [loading, isLoading] = useState(true);
 
-    useEffect(() => {
-        const getApi = async () => {
-            try {
-                const response = await fetch("/api/list/sellerList", {
-                    method: "GET",
-                    headers: {
-                        "Content-Type": "application/json"
-                    }
-
-                })
-                if (!response.ok) {
-                    throw new Error('Не удалось загрузить заказы');
-                }
-                const data = await response.json();
-                setOrders(data);
-                isLoading(false)
-            }catch(err) {
-                setError(err.message);
-                isLoading(false)
-                console.log(err)
+    const getApi = () => {
+       fetch("/api/list/sellerList", {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
             }
-        }
-        getApi()
-    }, [])
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
+    }
+    getApi();
 
     return (
-        <>
-            <Header/>
-            <div className="SellerAllOrdersPage">
-                {loading && <p>Загрузка заказов...</p>}
-                {error && <p>Ошибка: {error}</p>}
-                {!loading && !error && orders.length === 0 && <p>Заказы не найдены.</p>}
-                {!loading && !error && orders.length > 0 && (
-                    <ul>
-                        {orders.map((order) => (
-                            <li key={order.id}>
-                                ID заказа: {order.id} - Статус: {order.status}
-                            </li>
-                        ))}
-                    </ul>
-                )}
-            </div>
-            <Footer/>
-        </>
+        <div className="SellerAllOrdersPage">
+            123
+        </div>
     );
 };
