6a97520793e7fcd179b693ad7e1ae557b2489a69
Pavel <pavel20042019@mail.ru>
2025-05-17 12:49:21 +0700

    DateFix

--- src/modules/Pages/SellerPages/PatchOrderPage.jsx
+++ src/modules/Pages/SellerPages/PatchOrderPage.jsx
@@ -107,8 +107,7 @@ export const PatchOrderPage = () => {
                     const month = String(date.getMonth() + 1).padStart(2, '0');
                     const day = String(date.getDate()).padStart(2, '0');
                     refs.dateRef.current.value = `${year}-${month}-${day}`;
-                    setInputValue(prev => ({
-                        ...prev,
+                    setInputValue(() => ({
                         dateOrder: `${year}-${month}-${day}`
                     }));
                 },
