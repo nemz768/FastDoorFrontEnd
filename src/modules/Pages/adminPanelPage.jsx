65a628da76542e4c425699fe7765bff1a8ae7ae1
Pavel <pavel20042019@mail.ru>
2025-05-25 14:09:45 +0700

    adminPanelSearchTest

--- src/modules/Pages/adminPanelPage.jsx
+++ src/modules/Pages/adminPanelPage.jsx
@@ -33,7 +33,7 @@ export const AdminPanelPage = () => {
                     },
                 });
                 if (!response.ok) {
-                    throw new Error(`Не удалось загрузить заказы: ${response.statusText}`);
+                    throw new Error(`Не удалось загрузить заказы либо не существует!`);
                 }
                 const data = await response.json();
                 console.log('API data:', data);
@@ -72,23 +72,38 @@ export const AdminPanelPage = () => {
         setCurrentPage(currentPage);
     }
 
+    const cleanBtn = document.querySelector('#cleanBtn');
+
+    if (!nickName === '') {
+        cleanBtn.style.display = 'flex';
+    }else {
+        cleanBtn.style.display = 'none';
+    }
+
     return (
         <div className="admin-panel">
             <Header />
             <main className="SellerAllOrdersPage">
                 <div>
                     <h2>Панель администратора</h2>
-                    <input
-                        onChange={handleSearch}
-                        type="search"
-                        value={nickName}
-                        placeholder="Поиск по филиалу..."
-                    />
+                    <div className="InputBlock">
+                        <input
+                            onChange={handleSearch}
+                            type="search"
+                            value={nickName}
+                            placeholder="Поиск по филиалу..."
+                        />
+                        <button id="CleanButton" onClick={handleError} className='cleanInputSvg'>
+                            <svg className="crestSvg" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 50 50">
+                                <path d="M 40.783203 7.2714844 A 2.0002 2.0002 0 0 0 39.386719 7.8867188 L 25.050781 22.222656 L 10.714844 7.8867188 A 2.0002 2.0002 0 0 0 9.2792969 7.2792969 A 2.0002 2.0002 0 0 0 7.8867188 10.714844 L 22.222656 25.050781 L 7.8867188 39.386719 A 2.0002 2.0002 0 1 0 10.714844 42.214844 L 25.050781 27.878906 L 39.386719 42.214844 A 2.0002 2.0002 0 1 0 42.214844 39.386719 L 27.878906 25.050781 L 42.214844 10.714844 A 2.0002 2.0002 0 0 0 40.783203 7.2714844 z"></path>
+                            </svg>
+                        </button>
+                    </div>
                 </div>
                 {isLoading && <div className="loading">Загрузка...</div>}
                 {error && (
                     <div className="error">
-                        Ошибка: {error}
+                        <h3>Ошибка: {error}</h3>
                         <button onClick={handleError} className="retry-button">
                             Повторить
                         </button>
