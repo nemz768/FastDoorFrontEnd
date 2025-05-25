93db91b87711a85546008bc3a4914e926cc3fe6f
Pavel <pavel20042019@mail.ru>
2025-05-25 15:41:16 +0700

    adminPanelSearchTest

--- src/modules/Pages/adminPanelPage.jsx
+++ src/modules/Pages/adminPanelPage.jsx
@@ -72,7 +72,6 @@ export const AdminPanelPage = () => {
         setNickName('');
         setCurrentPage(0);
     }
-
     return (
         <div className="admin-panel">
             <Header />
@@ -89,7 +88,7 @@ export const AdminPanelPage = () => {
                         />
                         {showButtonClear && (
                             <button id="CleanButton" onClick={handleClearSearch} className='cleanInputSvg'>
-                                <svg className="crestSvg" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 50 50">
+                                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="17" height="17" viewBox="0 0 50 50">
                                     <path d="M 40.783203 7.2714844 A 2.0002 2.0002 0 0 0 39.386719 7.8867188 L 25.050781 22.222656 L 10.714844 7.8867188 A 2.0002 2.0002 0 0 0 9.2792969 7.2792969 A 2.0002 2.0002 0 0 0 7.8867188 10.714844 L 22.222656 25.050781 L 7.8867188 39.386719 A 2.0002 2.0002 0 1 0 10.714844 42.214844 L 25.050781 27.878906 L 39.386719 42.214844 A 2.0002 2.0002 0 1 0 42.214844 39.386719 L 27.878906 25.050781 L 42.214844 10.714844 A 2.0002 2.0002 0 0 0 40.783203 7.2714844 z"></path>
                                 </svg>
                             </button>
