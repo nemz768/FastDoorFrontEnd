ce84751c8f523c0b4576b45f4e1386c1c9eaf29b
Pavel <pavel20042019@mail.ru>
2025-05-25 16:31:37 +0700

    adminPanelSearchTest

--- src/modules/Main.jsx
+++ src/modules/Main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../styles/main.css';
 import { Popup } from "./special/Popup.jsx";
-
+import {imagefun} from '../assets/zzz.jpg'
 export const Main = () => {
     return (
         <div className="main">
             Main
+            <img src={imagefun} alt="mine"/>
             <Popup />
         </div>
     );
