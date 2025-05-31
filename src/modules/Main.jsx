b1a873aee3032dde3688d2ab8b313824083da3d5
Pavel <pavel20042019@mail.ru>
2025-05-31 16:54:12 +0700

    HEADER

--- src/modules/Main.jsx
+++ src/modules/Main.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import '../styles/main.css';
 import { Popup } from "./special/Popup.jsx";
-import imagefun from '../assets/zzz.jpg'
 export const Main = () => {
     return (
         <div className="main">
             Main
-            <img src={imagefun} alt="mine"/>
             <Popup />
         </div>
     );
