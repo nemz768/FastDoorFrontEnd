d53ca9692736c3996383f2821873700f6920c223
Pavel <pavel20042019@mail.ru>
2025-06-16 16:47:01 +0700

    test2

--- src/modules/Pages/MainInstallerPages/MainInstallerPage.jsx
+++ src/modules/Pages/MainInstallerPages/MainInstallerPage.jsx
@@ -3,6 +3,9 @@ import { Header } from '../../Header.jsx';
 import { Footer } from '../../Footer.jsx';
 import { CustomCalendar } from '../../special/CustomCalendar.jsx';
 import '../../../styles/styleMainInstaller/MainInstallerPage.css'
+import {shutdownSvg} from '../../../assets/shutdownsvg.svg'
+import {shutdownSvg} from '../../../assets/shutdownsvg.svg'
+import {shutdownSvg} from '../../../assets/shutdownsvg.svg'
 
 export const MainInstallerPage = () => {
     const [orders, setOrders] = useState([]);
@@ -412,14 +415,14 @@ export const MainInstallerPage = () => {
                             closedSelectedDates={closedSelectedDates}
                             setClosedSelectedDates={setClosedSelectedDates}
                         />
-                        <button onClick={closeDateCalendar} disabled={!selectedDate || isAvailabilityChanging}>
-                            {isAvailabilityChanging ? "Закрытие..." : "Закрыть день!"}
+                        <button className="Calendar-Button-MainInstaller" onClick={closeDateCalendar} disabled={!selectedDate || isAvailabilityChanging}>
+                            {isAvailabilityChanging ? shutdownSvg : "Закрыть день!"}
                         </button>
-                        <button  onClick={openDates}
+                        <button className="Calendar-Button-MainInstaller" onClick={openDates}
                                  disabled={closedSelectedDates.size === 0 || isAvailabilityChanging}>
                             {isAvailabilityChanging ? "Открытие..." : "Открыть день"}
                         </button>
-                        <button>
+                        <button className="Calendar-Button-MainInstaller">
                             Изменить количество дверей
                         </button>
                     </div>
