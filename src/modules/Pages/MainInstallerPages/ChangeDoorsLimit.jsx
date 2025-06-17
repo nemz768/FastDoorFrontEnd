8c3867eee757a8da9015e7f65ad9825147656f89
Pavel <pavel20042019@mail.ru>
2025-06-17 17:01:51 +0700

    ChangeDataOfCalendar

--- src/modules/Pages/MainInstallerPages/ChangeDoorsLimit.jsx
+++ src/modules/Pages/MainInstallerPages/ChangeDoorsLimit.jsx
@@ -4,38 +4,34 @@ import '../../../styles/styleMainInstaller/ChangeDoorLimits.css'
 
 export const ChangeDoorsLimit = ({selectedDate, setOpenCalendarDateChange}) => {
 
-
-
-
     const DoorLimits = {
         frontDoorQuantity: useRef(null),
         inDoorQuantity: useRef(null)
     }
-    const patchDoorLimits = async ()=> {
+    const patchDoorLimits = async () => {
         const frontDoorQuantity = DoorLimits.frontDoorQuantity.current.value;
         const inDoorQuantity = DoorLimits.inDoorQuantity.current.value;
+
         try {
-            const response = await fetch("api/doorLimits/editDate", {
+            const response = await fetch("/api/doorLimits/editDate", {
                 method: 'PATCH',
                 headers: {
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify({
                     date: selectedDate,
-                    inDoorQuantity: inDoorQuantity,
-                    frontDoorQuantity: frontDoorQuantity
+                    frontDoorQuantity: Number(frontDoorQuantity),
+                    inDoorQuantity: Number(inDoorQuantity),
+                    available: true
                 })
-            })
+            });
 
-           const data =  await response.text();
-            console.log(data)
-
+            const data = await response.text();
+            console.log(data);
+        } catch (error) {
+            console.log("Ошибка:", error);
         }
-        catch (error) {
-            console.log(error)
-        }
-
-    }
+    };
 
 
     const handleConfirm = () =>  {
@@ -54,9 +50,10 @@ export const ChangeDoorsLimit = ({selectedDate, setOpenCalendarDateChange}) => {
 
             <div>
                 <p>Входные двери: </p>
-                <input ref={DoorLimits.inDoorQuantity} type="text"/>
+                <input ref={DoorLimits.frontDoorQuantity} type="text" />
+
                 <p>Межкомнатные двери: </p>
-                <input ref={DoorLimits.frontDoorQuantity} type="text"/>
+                <input ref={DoorLimits.inDoorQuantity} type="text" />
                 <button onClick={handleConfirm}>Подтвердить</button>
             </div>
         </div>
