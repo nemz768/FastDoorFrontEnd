f0b480c0576129f199b90babde9cc5f983b17dfe
Pavel <pavel20042019@mail.ru>
2025-08-02 00:01:14 +0700

    styles-fixes mainInstaller v1.0.1

--- src/modules/Api/getSessionApi/GetSessionApi.tsx
+++ src/modules/Api/getSessionApi/GetSessionApi.tsx
@@ -3,31 +3,31 @@ import { useNavigate } from "react-router-dom";
 
 
 export const GetSessionApi = () => {
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        const checkSession = async () => {
-            try {
-                const response = await fetch('/api/check-session', {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                });
-
-                const data = await response.json();
-
-                if (data.status === 401) {
-                    navigate('/login');
-                }
-            } catch (err) {
-                console.error("Ошибка проверки сессии:", err);
-                navigate('/login');
-            }
-        };
-
-        checkSession();
-    }, [navigate]);
+    // const navigate = useNavigate();
+    //
+    // useEffect(() => {
+    //     const checkSession = async () => {
+    //         try {
+    //             const response = await fetch('/api/check-session', {
+    //                 method: 'GET',
+    //                 headers: {
+    //                     'Content-Type': 'application/json'
+    //                 }
+    //             });
+    //
+    //             const data = await response.json();
+    //
+    //             if (data.status === 401) {
+    //                 navigate('/login');
+    //             }
+    //         } catch (err) {
+    //             console.error("Ошибка проверки сессии:", err);
+    //             navigate('/login');
+    //         }
+    //     };
+    //
+    //     checkSession();
+    // }, [navigate]);
 
     return null;
 };
