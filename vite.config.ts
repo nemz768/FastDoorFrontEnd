7c1ac8606f67917189d6cb37cc13f4e848688341
Pavel <pavel20042019@mail.ru>
2025-09-19 10:17:49 +0700

    seo plugin check v1

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import vitePrerender from 'vite-plugin-prerender';
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    vitePrerender({
+      staticDir: path.resolve(__dirname, 'dist'),
+      routes: ['/login',],  // список маршрутов для пререндеринга
+      // другие настройки, например, задержка, postProcess и др.
+    })
+  ],
   server: {
     port: 8081,
   },
